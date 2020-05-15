6aaea914251b6b0ee6f3b0109cd62194f58c0081
Fabio <fabiomac@gmail.com>
2020-05-14 21:17:14 -0300

    Definição das Views e suas Rotas(urls)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World. Estamos no index de Polls-Teste reload")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking a question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
