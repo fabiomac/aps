80b93de8b821ab1162301c7792fabd518a7037b2
Fabio <fabiomac@gmail.com>
2020-05-21 20:25:50 -0300

    Tutorial django parte 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,12 +8,15 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:7]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,}
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking a question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
